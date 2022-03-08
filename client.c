9aa8a6c4b811c5bd649fa3c26cd1c3f548094d75
cacharri <82544904+cacharri@users.noreply.github.com>
2022-03-08 21:10:42 +0100

    hello

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 18:18:41 by ialvarez          #+#    #+#             */
-/*   Updated: 2022/03/07 21:16:52 by ialvarez         ###   ########.fr       */
+/*   Updated: 2022/03/08 21:09:39 by ialvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ static void	writting(int pid, char br)
 	int	i;
 
 	i = 0;
-	while (i++ < 8)
+	while (i < 8)
 	{
-		if (((br >> i) & 1) == 0)
+		if (((br >> i++) & 0x01) == 0)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(150);
+		usleep(100);
 	}
 }
 
@@ -38,10 +38,7 @@ int main(int argc, char **argv)
 	{
 		PID = ft_atoi(argv[1]);
 		while (argv[2][i] != '\0')
-		{
-			writting(PID, argv[2][i]);
-			i++;
-		}
+			writting(PID, argv[2][i++]);
 	}
 	writting(PID, '\0');
 	return (0);
--- server.c
+++ server.c
@@ -6,79 +6,37 @@
 /*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 18:19:51 by ialvarez          #+#    #+#             */
-/*   Updated: 2022/03/07 21:16:50 by ialvarez         ###   ########.fr       */
+/*   Updated: 2022/03/08 21:09:48 by ialvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static char *str_add_one(char q)
+int count = 0;
+
+static void	copy(int bit)
 {
-	char *add;
+	static char x;
 
-	add = (char *)malloc(sizeof(char) * 2);
-	if (!add)
-		return (NULL);
-	add[0] = q;
-	add[1] = '\0';
-	return (add);
-}
-
-static char *str_add(char *uwu, char q)
-{
-	char	*add;
-	int		i;
-
-	if (!q)
-		return (NULL);
-	if (!uwu)
-		return (str_add_one(q));
-	add = (char *)malloc(sizeof(char) * (ft_strlen(uwu) + 2));
-	if (!add)
+	if (count == 0)
+		x = 0;
+	x += bit << count;
+	count++;
+	if (count == 8)
 	{
-		free(uwu);
-		return (NULL);
+		ft_putstr_fd(&x, 1);
+		if (x == '\0')
+			ft_putstr_fd("\n", 1);
+		count = 0;
 	}
-	i = 0;
-	while (uwu[i++])
-		add[i] = uwu[i];
-	free(uwu);
-	add[i] = q;
-	add[++i] = '\0';
-	return (add);
 }
 
-static char *print(char *mes)
+static	void	handle(int sig)
 {
-	ft_putstr_fd(mes, 1);
-	free(mes);
-	return (NULL);
-}
-
-void send_pid(int a, siginfo_t *t, void *lux)
-{
-	static int lock;
-	static char c;
-	static int bit;
-	int pid;
-	static char *write;
-
-	(void)lux;
-	c = 0xFF;
-	pid = t->si_pid;
-	if (a == SIGUSR1)
-		c ^= 0x80 >> bit;
-	else if (a == SIGUSR2)
-		c |= 0x80 >> bit;
-	if (++bit == 8)
-	{
-		if (c)
-			write = str_add(write, c);
-		else
-			write = print(write);
-		bit = 0;
-		c = 0xFF;
-	}
+	if (sig == 30)
+		copy(0);
+	if (sig == 31)
+		copy(1);
 }
 
 int	main(void)
@@ -90,15 +48,16 @@ int	main(void)
 	sigemptyset(&block);
 	sigaddset(&block, SIGINT);
 	sigaddset(&block, SIGQUIT);
-	sig.sa_handler = 0;
+	sig.sa_handler = &handle;
 	sig.sa_flags = SA_SIGINFO;
-	sig.sa_sigaction = send_pid;
+	sig.sa_mask = block;
 	sigaction(SIGUSR1, &sig, NULL);
 	sigaction(SIGUSR2, &sig, NULL);
 	ft_putstr_fd("PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putstr_fd("\n", 1);
 	while (1)
+	{
 		pause();
-	return (0);
+	}
 }
